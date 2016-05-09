959abbd9813ec2feca255de33f49b13920191e88
MANOJ G <mailmrmanoj@users.noreply.github.com>
2016-05-09 18:30:41 +0530

    Update App.ts

--- app/scripts/App.ts
+++ app/scripts/App.ts
@@ -15,8 +15,6 @@ module AngularAttack {
             var bootstraper = new AngularAttack.Bootstrapper(this.module, router);
             bootstraper.bootstrap();
             this.setModuleUrlConstants();
-
-            // this.module.value(commonConstants.CONFIG_CONSTANT, window.reactoreConfigurations.applicationConfig);
         }
         setModuleUrlConstants = () => {
             $.ajax({
@@ -34,9 +32,3 @@ module AngularAttack {
     };
     new App();
 }
-// Date.prototype.yyyymmdd = function () {
-//     var yyyy = this.getFullYear().toString();
-//     var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
-//     var dd = this.getDate().toString();
-//     return yyyy + '-' + (mm[1] ? mm : "0" + mm[0]) + '-' + (dd[1] ? dd : "0" + dd[0]); // padding
-// };
